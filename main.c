cbade125b74aa338300a410c22bbe1ce0c8b3627
Dominique <dmegnidro@gmail.com>
2023-03-06 04:07:54 +0100

    seconde commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int jouer_colonne(Grille* g, int joueur, int colonne, Joueur j1, Joueur j2) {
+    char symbole = (joueur == 1) ? j1.symbole : j2.symbole;
+    for (int i = NB_LIGNES-1; i >= 0; i--) {
+        if (g->grille[i][colonne] == ' ') {
+            g->grille[i][colonne] = symbole;
+            return 1;
+        }
+    }
+    printf("La colonne est pleine, veuillez jouer ailleurs.\n");
+    return 0;
+}
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
